0ad755f1c07e7a3e390400127d254bd23aa5aff6
Dushyant Surya <djdushyantsurya@gmail.com>
2018-03-25 12:18:33 +0530

    Add Delete Script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,21 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db)=> {
+  if(err)
+  {
+    return  console.log('Unable to connect to mongoDB server');
+  }
+
+  db.collection('Users').deleteMany({
+    name: 'Dushyant Surya'
+  }).then((result)=> {
+    console.log(result);
+  })
+
+  db.collection('Users').findOneAndDelete({
+    _id: ObjectID("5ab744cb13c9619dad852caf")
+  }).then((result) => {
+    console.log(result);
+  })
+
+})
