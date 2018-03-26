74e545e39d5e3a2a736d6a53a49d5193d6118e27
Dushyant Surya <djdushyantsurya@gmail.com>
2018-03-26 08:15:13 +0530

    Add GET/todos route

--- server/server.js
+++ server/server.js
@@ -22,7 +22,16 @@ app.post('/todos', (req, res)=>{
   });
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  }, (e)=> {
+    res.send(e);
+  })
+})
 
 app.listen(3000, ()=> {
   console.log('Setting up server at port');
-})
+});
+
+module.exports = {app};
